2601e68e57f0de20e3e2d62256a92e68a15471a3
Vitor de Paula Santos <vitorpaulasantos@gmail.com>
2023-03-29 17:57:30 -0300

    variables and your types

--- go-1/src/hello/hello.go
+++ go-1/src/hello/hello.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	var name = "Vitor"
+	var age = 20
+	var version float32 = 1.1
+
+	fmt.Println("Ola, Sr", name, "sua idade é", age)
+	fmt.Println("Esta na versão ", version)
+
+	fmt.Println("O tipo da variavel", name, "é", reflect.TypeOf(name))
+	fmt.Println("O tipo da variavel", age, "é", reflect.TypeOf(age))
+	fmt.Println("O tipo da variavel", version, "é", reflect.TypeOf(version))
 }
