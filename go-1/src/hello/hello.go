5289803e0b9c5f51f5fbc4a45981310f068f7fb0
Vitor de Paula Santos <vitorpaulasantos@gmail.com>
2023-03-31 08:44:32 -0300

    intro of functions in GoLang

--- go-1/src/hello/hello.go
+++ go-1/src/hello/hello.go
@@ -2,41 +2,47 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
+
+	intro()
+	showMenu()
+	command := readCommand()
+
+	switch command {
+	case 1:
+		fmt.Println("Monitorando")
+	case 2:
+		fmt.Println("Exibindo logs")
+	case 0:
+		fmt.Println("Saindo ...")
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheço este comando!")
+		os.Exit(-1)
+	}
+}
+
+func intro() {
 	name := "Vitor"
 	version := 1.1
 
 	fmt.Println("Ola, Sr", name)
-	fmt.Println("Esta na versão ", version)
+	fmt.Println("Este programa esta na versão", version)
+}
 
+func showMenu() {
 	fmt.Println("1 - Iniciar MOnitoramento")
 	fmt.Println("2 - Exibir logs")
 	fmt.Println("0 - Sair")
+}
 
-	var command int
-	fmt.Scan(&command)
-	fmt.Println("O comando escolhido foi", command)
+func readCommand() int {
+	var readedCommand int
+	fmt.Scan(&readedCommand)
+	fmt.Println("O comando escolhido foi", readedCommand)
 
-	//if command == 1 {
-	//	fmt.Println(" Monitorando")
-	//} else if command == 2 {
-	//	fmt.Println(" Exibindo logs")
-	//} else if command == 0 {
-	//	fmt.Println(" Saindo")
-	//} else {
-	//	fmt.Println("Não conheço este comando!")
-	//}
-
-	switch command {
-	case 1:
-		fmt.Println(" Monitorando")
-	case 2:
-		fmt.Println(" Exibindo logs")
-	case 0:
-		fmt.Println(" Saindo")
-	default:
-		fmt.Println("Não conheço este comando!")
-	}
+	return readedCommand
 }
