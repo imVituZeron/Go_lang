cef96fa7443929bab0aa28e4b6dbc47a39fd6825
Vitor de Paula Santos <vitorpaulasantos@gmail.com>
2023-04-07 09:49:27 -0300

    implementing the function sacar()

--- go-2/src/banco/main.go
+++ go-2/src/banco/main.go
@@ -9,17 +9,25 @@ type ContaCorrente struct { // estrutura == classe
 	saldo      float64
 }
 
+func (c *ContaCorrente) sacar(saque float64) string {
+	positivo := saque > 0 && saque < c.saldo
+
+	if positivo {
+		c.saldo -= saque
+		return "saque feito com sucesso!"
+	} else {
+		return "Saldo insuficiente!"
+	}
+}
+
 func main() {
 	//conta1 := ContaCorrente{titular: "guilherm", numAgencia: 589, numConta: 5, saldo: 123.45} forma não obriga passar todos os params
 	//conta1 := ContaCorrente{"guilherm", 589, 5, 123.45} forma obriga passar todos os params
 	conta1 := ContaCorrente{}
-	conta1.titular = "Guilherme"
-	conta1.numAgencia = 568
 	conta1.numConta = 05
 	conta1.saldo = 123.35
 
-	fmt.Println(conta1.titular)
-	fmt.Println(conta1.numAgencia)
-	fmt.Println(conta1.numConta)
+	fmt.Println(conta1.saldo)
+	fmt.Println(conta1.sacar(60))
 	fmt.Println(conta1.saldo)
 }
