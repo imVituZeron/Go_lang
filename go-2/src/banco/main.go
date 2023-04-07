3aff521c7d4b21b9fb31500718b36c3d73931a2e
Vitor de Paula Santos <vitorpaulasantos@gmail.com>
2023-04-06 21:20:45 -0300

    starting in the course GOOO

---
+++ go-2/src/banco/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	titular := "Vitor"
+	numAgencia := 598
+	numConta := 123456
+	saldo := 125.50
+
+	fmt.Println("Titular:", titular)
+	fmt.Println("Agência:", numAgencia)
+	fmt.Println("Conta", numConta)
+	fmt.Println("Saldo", saldo)
+}
