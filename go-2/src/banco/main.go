6b8e44184048cf4418ce9431dffa1ef991ef2de6
Vitor de Paula Santos <vitorpaulasantos@gmail.com>
2023-04-10 08:05:09 -0300

    creating the struct cliente and saldo private

--- go-2/src/banco/main.go
+++ go-2/src/banco/main.go
@@ -3,17 +3,15 @@ package main
 import (
 	"fmt"
 
-	c "pack.com/go-2/pkg/contas" // o "c" é uma apelido para os import
+	cli "pack.com/go-2/pkg/clientes" // o "c" é uma apelido para os import
+	c "pack.com/go-2/pkg/contas"
 )
 
 func main() {
-	//conta1 := ContaCorrente{titular: "guilherm", numAgencia: 589, numConta: 5, saldo: 123.45} forma não obriga passar todos os params
-	//conta1 := ContaCorrente{"guilherm", 589, 5, 123.45} forma obriga passar todos os params
-	conta1 := c.ContaCorrente{Titular: "Vitor", Saldo: 1000}
-	conta2 := c.ContaCorrente{Titular: "Sara", Saldo: 6000}
+	clienteVitor := cli.Titular{Nome: "Vitor", CPF: "23165498744", Profissao: "Analista DevOps"}
+	contaDoVitor := c.ContaCorrente{Titular: clienteVitor, NumAgencia: 123, NumConta: 123456}
 
-	fmt.Println(conta1, conta2)
-	status := conta1.Transferir(100, &conta2)
-	fmt.Println(status, conta1, conta2)
+	contaDoVitor.Depositar(1000)
+	fmt.Println(contaDoVitor.Visulizar())
 
 }
