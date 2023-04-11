eda3419351acbdb50bca1c6f0ea653d5a0cd6e1e
Vitor de Paula Santos <vitorpaulasantos@gmail.com>
2023-04-11 11:39:57 -0300

    creating the interface verificaConta for buy tickets

--- go-2/src/banco/main.go
+++ go-2/src/banco/main.go
@@ -7,20 +7,26 @@ import (
 	c "pack.com/go-2/pkg/contas"
 )
 
+func PagarBoleto(conta verificarConta, valorBoleto float64) {
+	conta.Sacar(valorBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
 	clienteVitor := cli.Titular{Nome: "Vitor", CPF: "23165498744", Profissao: "Analista DevOps"}
 	contaCorrDoVitor := c.ContaCorrente{Titular: clienteVitor, NumAgencia: 123, NumConta: 123456}
 	contaPoupDoVitor := c.ContaPoupanca{Titular: clienteVitor, NumAgencia: 123, NumConta: 123654}
 
-	contaCorrDoVitor.Depositar(1000)
-	fmt.Println(contaCorrDoVitor.Visulizar())
-	fmt.Println(contaCorrDoVitor)
+	contaCorrDoVitor.Depositar(100)
 
-	contaPoupDoVitor.Depositar(1000)
+	PagarBoleto(&contaCorrDoVitor, 60)
+
+	contaPoupDoVitor.Depositar(100)
+	PagarBoleto(&contaPoupDoVitor, 25)
+
+	fmt.Println(contaCorrDoVitor.Visulizar())
 	fmt.Println(contaPoupDoVitor.Visulizar())
-	fmt.Println(contaPoupDoVitor)
-
-	contaCorrDoVitor.Sacar(55)
-	fmt.Println(contaCorrDoVitor.Visulizar())
-
 }
