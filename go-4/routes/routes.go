6fc5f1966c5724a99d7926e977de703f267359fe
imVituZeron <vitorpaulasantos@gmail.com>
2023-05-06 14:52:25 -0300

    exchanging the library of routes

--- go-4/routes/routes.go
+++ go-4/routes/routes.go
@@ -5,10 +5,13 @@ import (
 	"net/http"
 
 	control "pack/api/controller"
+
+	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
-	http.HandleFunc("/", control.Home)
-	http.HandleFunc("/api/personalidades", control.ShowAllPersonas)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", control.Home)
+	r.HandleFunc("/api/personalidades", control.ShowAllPersonas)
+	log.Fatal(http.ListenAndServe(":8081", r))
 }
