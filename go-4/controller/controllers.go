f0a07909425f6c4c3fcea04c024e2c53638e2fd1
imVituZeron <vitorpaulasantos@gmail.com>
2023-05-28 14:06:35 -0300

    finished CRUD

--- go-4/controller/controllers.go
+++ go-4/controller/controllers.go
@@ -29,3 +29,31 @@ func ShowPersona(w http.ResponseWriter, r *http.Request) {
 	database.DB.First(&P, id)
 	json.NewEncoder(w).Encode(P)
 }
+
+func CreateNewPersona(w http.ResponseWriter, r *http.Request) {
+	var novaPersonalidade models.Personalidade
+	json.NewDecoder(r.Body).Decode(&novaPersonalidade)
+
+	database.DB.Create(&novaPersonalidade)
+	json.NewEncoder(w).Encode(novaPersonalidade)
+}
+
+func DeletePersona(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personalidade models.Personalidade
+
+	database.DB.Delete(&personalidade, id)
+	json.NewEncoder(w).Encode(personalidade)
+}
+
+func EditPersona(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personalidade models.Personalidade
+	database.DB.First(&personalidade, id)
+
+	json.NewDecoder(r.Body).Decode(&personalidade)
+	database.DB.Save(&personalidade)
+	json.NewEncoder(w).Encode(personalidade)
+}
--- go-4/routes/routes.go
+++ go-4/routes/routes.go
@@ -14,5 +14,8 @@ func HandleRequest() {
 	r.HandleFunc("/", control.Home)
 	r.HandleFunc("/api/personalidades", control.ShowAllPersonas).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", control.ShowPersona).Methods("Get")
+	r.HandleFunc("/api/personalidades", control.CreateNewPersona).Methods("Post")
+	r.HandleFunc("/api/personalidades/{id}", control.DeletePersona).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", control.EditPersona).Methods("Put")
 	log.Fatal(http.ListenAndServe(":8081", r))
 }
