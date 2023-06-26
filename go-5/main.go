f49d01c2dfb45bfef51f84084b037d8002837bc6
imVituZeron <vitorpaulasantos@gmail.com>
2023-06-26 07:40:43 -0300

    creating first route

--- go-5/main.go
+++ go-5/main.go
@@ -2,7 +2,15 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+func showStudents(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"id":   "1",
+		"name": "Vitor Santos",
+	})
+}
+
 func main() {
 	r := gin.Default()
+	r.GET("/alunos", showStudents)
 	r.Run(":5000")
 }
